ad529db7f429bdc6406e64fac217d163e41a6896
Steve Benchik <stevebenchik@gmail.com>
2018-07-10 13:25:18 -0500

    Add new route for login form

--- server.py
+++ server.py
@@ -6,3 +6,7 @@ app = Flask(__name__, static_url_path="")
 @app.route('/')
 def index():
     return app.send_static_file('index.html')
+
+@app.route('/login')
+def login_page():
+    return app.send_static_file('signup.html')

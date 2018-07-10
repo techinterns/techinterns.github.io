7f7f7fbc21ec1a8c698aad97254fb7dc96ad15ac
Steve Benchik <stevebenchik@gmail.com>
2018-07-10 12:11:43 -0500

    Add basic flask server

---
+++ server.py
@@ -0,0 +1,8 @@
+from flask import Flask
+from flask import render_template
+
+app = Flask(__name__, static_url_path="")
+
+@app.route('/')
+def index():
+    return app.send_static_file('index.html')

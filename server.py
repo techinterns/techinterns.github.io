108fd05b6e239967c145a069d10edc3af162258f
Steve Benchik <stevebenchik@gmail.com>
2018-07-16 13:49:42 -0500

    Connect server to SQLite database and create dummy table/user

--- server.py
+++ server.py
@@ -1,7 +1,16 @@
 from flask import Flask
-from flask import render_template
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
 app = Flask(__name__, static_url_path="")
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///buddy.db'
+db = SQLAlchemy(app)
+
+class User(db.Model):
+    email = db.Column(db.String(120), unique=True, primary_key=True)
+
+    def __repr__(self):
+        return 'User {}'.format(self.email)
 
 @app.route('/')
 def index():
